e68a22e9eef7c420012ecd91e24c13e9b75ad12e
LJTDiSZ <jiang.github@163.com>
2016-03-01 17:30:38 +0800

    Tongyin initial

---
+++ WebSite1/tongyin/js/app.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    console.log($('.topBar li a').height());
+
+    $(".pagesContianer").show();
+
+    $('.pagesContianer').fullpage();
+    // $.fn.fullpage.reBuild();
+    $(".fp-section").css({ //修正兼容性
+        height: window.document.body.clientHeight + "px"
+    });
+
+});
